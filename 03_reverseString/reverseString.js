b7ca888d3f5e7edd93a5c43e7f8b6926834eb9a2
NickMusienko <nickm905@gmail.com>
2025-01-29 21:49:19 -0500

    Completed 03 and 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    tempStr = "";
+    for (let i = str.length; i >= 0; i--) {
+        tempStr += str.charAt(i);
+    }
+    return tempStr;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrSubject, ...arrRest) {
+    let arrTemp = arrSubject.slice();
+    for (let i = 0; i <= arrRest.length; i++) {
+        let idx = arrTemp.indexOf(arrRest[i]);
+        while (idx > -1) {
+            arrTemp.splice(idx, 1);
+            idx = arrTemp.indexOf(arrRest[i]);
+        }
+    }
+
+    return arrTemp;
 };
 
 // Do not edit below this line
