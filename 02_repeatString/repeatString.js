5ac233b1e47a37b4d1567c94ca022df978a0e28c
NickMusienko <nickm905@gmail.com>
2025-01-29 21:27:39 -0500

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let tempStr = "";
+    if (num < 0) {
+        tempStr = "ERROR"
+    }
+    else {
+        for (let i = 1; i <= num; i++) {
+            tempStr += string;
+        }
+    };
+    return tempStr;
 };
 
 // Do not edit below this line
